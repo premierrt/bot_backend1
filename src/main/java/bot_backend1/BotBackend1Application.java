f61eef17ff488d2e35943116886d5ee1fb978eda
Rafał <rafal@xxx.xx>
2018-06-06 14:05:05 +0200

    1

---
+++ src/main/java/bot_backend1/BotBackend1Application.java
@@ -0,0 +1,12 @@
+package bot_backend1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BotBackend1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BotBackend1Application.class, args);
+	}
+}
